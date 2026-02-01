19a98012ed82620c37c974f9dbfc5563171d72ae
saifalafeefi <saif.alafeefi2003@gmail.com>
2026-02-01 16:04:34 +0400

    0.5.0
    - added custom enemy idle and run sprites
    - updated player controller to improve transition speeds between attacks
    - updated sprite flipping for enemy animation controller
    - removed old sprites and animations
    
    Co-Authored-By: FahadAlhashimi99 <221425998+FahadAlhashimi99@users.noreply.github.com>
    Co-Authored-By: Mohamed Al Mehairbi <62325490+itisrain@users.noreply.github.com>

--- Assets/Scripts/Mechanics/AnimationController.cs
+++ Assets/Scripts/Mechanics/AnimationController.cs
@@ -63,9 +63,9 @@ namespace Platformer.Mechanics
             }
 
             if (move.x > 0.01f)
-                spriteRenderer.flipX = false;
-            else if (move.x < -0.01f)
                 spriteRenderer.flipX = true;
+            else if (move.x < -0.01f)
+                spriteRenderer.flipX = false;
 
             animator.SetBool("grounded", IsGrounded);
             animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
